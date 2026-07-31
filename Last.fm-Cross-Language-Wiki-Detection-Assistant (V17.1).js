b8c9738e2edd9894c8ca00ca6d91170ee0528c23
Dgnic <75353499+Dgnic@users.noreply.github.com>
2026-07-31 16:08:24 +0800

    Update Last.fm-Cross-Language-Wiki-Detection-Assistant (V17.1).js

--- Last.fm-Cross-Language-Wiki-Detection-Assistant (V17.1).js
+++ Last.fm-Cross-Language-Wiki-Detection-Assistant (V17.1).js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Last.fm 跨语言维基跳转助手 (V17.1 - 增强维基触发)
+// @name         Last.fm-Cross-Language-Wiki-Detection-Assistant
 // @namespace    http://tampermonkey.net/
 // @version      17.1
 // @description  此脚本可自动检测 Last.fm 上缺失维基的专辑或音乐人页，并查找其他语言可用的维基链接。This script automatically detects album or artist pages with no wiki on Last.fm and finds wiki links in other languages.
